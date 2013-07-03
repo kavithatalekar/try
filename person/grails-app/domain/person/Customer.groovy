e5bbd9908f0d9d39998d3bc49bd2fecc45a25813
Namrata-Chavan <namrata.chavan23@gmail.com>
2013-07-03 12:03:39 +0530

    new attribute

--- person/grails-app/domain/person/Customer.groovy
+++ person/grails-app/domain/person/Customer.groovy
@@ -5,12 +5,13 @@ class Customer {
 	String name1
 	Integer age1
 	String address
-
+    String email
     static constraints = {
 		
 		name1(blank:false, nullable:false,size:3..30,matches:"[a-zA-z]+")
 		age1(blank:false)
 		address(blank:false)
+		email (email:true)
     }
 	
 	def String toString()
