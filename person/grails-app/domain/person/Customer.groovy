0cb925e011f42aa23eb1eb48d23bdcd29be6d6e0
Namrata-Chavan <namrata.chavan23@gmail.com>
2013-07-03 12:19:26 +0530

    name change

--- person/grails-app/domain/person/Customer.groovy
+++ person/grails-app/domain/person/Customer.groovy
@@ -5,7 +5,8 @@ class Customer {
 	String name1
 	Integer age1
 	String address
-    String email
+    String email1
+	
     static constraints = {
 		
 		name1(blank:false, nullable:false,size:3..30,matches:"[a-zA-z]+")
