f94495eb5182f55b9679beb49e646ac65bb17fb3
Namrata-Chavan <namrata.chavan23@gmail.com>
2013-07-03 12:26:28 +0530

    change in conflict

--- person/grails-app/domain/person/Customer.groovy
+++ person/grails-app/domain/person/Customer.groovy
@@ -3,21 +3,21 @@ package person
 class Customer {
 	
 	String name1
-<<<<<<< HEAD
+
 	String usn
 	Integer age
-=======
+
 	Integer age1
->>>>>>> branch 'master' of http://github.com/kavithatalekar/try.git
+
 	String address
-<<<<<<< HEAD
+
 	String phone
 	
 
-=======
+
     String email1
 	
->>>>>>> branch 'master' of http://github.com/kavithatalekar/try.git
+
     static constraints = {
 		
 		name1(blank:false, nullable:false,size:3..30,matches:"[a-zA-z]+")
